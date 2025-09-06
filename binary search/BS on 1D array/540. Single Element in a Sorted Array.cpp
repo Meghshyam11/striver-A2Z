976379f45e8a3f9ad27b223037980e744b662886
Meghshyam <meghshyamkarpe135711@gmail.com>
2025-09-06 13:35:02 +0530

    6 sep commit

---
+++ binary search/BS on 1D array/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left=0;
+        int right=nums.size()-1;
+
+        while(left<right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]%2==1){
+                mid--;
+            }
+            if(nums[mid]%2==nums[mid+1]){
+                left=mid+2;
+            }else{
+                right=mid;
+            }
+        }
+        return nums[left];
+    }
+};
